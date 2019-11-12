fc159536e92c5add062385a98f7b0961659b1d5b
cliffjsgit <32808702+cliffjsgit@users.noreply.github.com>
2019-11-12 12:49:18 -0600

    Add files via upload

---
+++ project_HTML.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+#
+# Example project for scraping and saving images from a Web site
+#
+
+from bs4 import BeautifulSoup
+import os
+import requests
+
+URL = "http://www.energyhive.com/"
+#URL = "https://www.python.org/"
+
+print("\nHTML Parsing", URL)
+
+page = requests.get(URL).text
+soup = BeautifulSoup(page, 'html.parser')
+
+current = os.listdir(path='.')
+
+for item in soup.find_all('img'):
+    print("  Image:", item.get('src').split('/')[1])
+    if item.get('src').split('/')[1] not in current:
+        r = requests.get(URL + item.get('src'), allow_redirects=True)
+        open(item.get('src').split('/')[1] , 'wb').write(r.content)
+        print('    Downloaded ' + item.get('src').split('/')[1])
+    else:
+        print('    Already downloaded ' + item.get('src').split('/')[1])
